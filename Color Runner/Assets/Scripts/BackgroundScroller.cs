b8250ca10e6b303be0ce0d2663b8e64979276e06
Kelly <kmmccallan@gmail.com>
2017-03-26 21:28:41 -0400

    Added and Attached Scripts to Objects
    Need to fix the jumping character movement, spawning scripts, general scripting errors, scroller background
    ALSO add the get ignore file for the library/assets, need to add gems, menu buttons, score in upper right hand corner
    NEED: photoshop files of the main character, gems without the white background, animation for new character

---
+++ Color Runner/Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour {
+    public float speed = 0;
+    public static BackgroundScroller current;
+
+    float pos = 0;
+
+	// Use this for initialization
+	void Start () {
+        current = this;
+	}
+    void Update ()
+    {
+        pos += speed;
+        if (pos > 1.0f)
+            pos -= 1.0f;
+
+        GetComponent<Renderer>().material.mainTextureOffset = new Vector2(pos, 0);
+
+    }
+	
+}
